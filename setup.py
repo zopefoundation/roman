6ccfc571030c9e70d81e11e7a102929098f30bf5
Stephan Richter <stephan.richter@gmail.com>
2009-07-23 16:25:57 +0000

    Import code and setup.

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup (
+    name='roman',
+    version='1.4.0',
+    author = "Mark Pilgrim",
+    author_email = "f8dy@diveintopython.org",
+    description = "Integer to Roman numerals converter",
+    license = "Python 2.1.1",
+    keywords = "roman",
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Python License',
+        'Programming Language :: Python',
+        'Natural Language :: English',
+        'Operating System :: OS Independent'],
+    url = 'http://pypi.python.org/pypi/roman',
+    include_package_data = True,
+    zip_safe = True,
+    )
