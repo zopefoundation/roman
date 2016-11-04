a3f040d5bfcca95ad91ae66750d4191c6b970c3c
Gil Forcada Codinachs <gil.gnome@gmail.com>
2016-11-04 23:49:49 +0100

    Add python 3.5 trove classifier

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ setup (
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
         'License :: OSI Approved :: Python Software Foundation License',
