d64b91b4a2501fb6193ad6c0ead431739d8dd6b5
Jens Vagelpohl <jens@netz.ooo>
2019-04-14 12:50:17 -0500

    - expanded test coverage

--- src/tests.py
+++ src/tests.py
@@ -1,21 +1,27 @@
 import unittest
 import roman
 
+TEST_MAP = ((0, 'N'), (1, 'I'), (3, 'III'), (4, 'IV'), (9, 'IX'), (14, 'XIV'),
+            (19, 'XIX'), (24, 'XXIV'), (40, 'XL'), (49, 'XLIX'), (90, 'XC'),
+            (99, 'XCIX'), (400, 'CD'), (490, 'CDXC'), (499, 'CDXCIX'),
+            (900, 'CM'), (990, 'CMXC'), (998, 'CMXCVIII'), (999, 'CMXCIX'),
+            (2013, 'MMXIII'))
+
 class TestRoman(unittest.TestCase):
 
     def test_toRoman(self):
-        self.assertEqual(roman.toRoman(0), 'N')
-        self.assertEqual(roman.toRoman(1), 'I')
-        self.assertEqual(roman.toRoman(2013), 'MMXIII')
+        for num_arabic, num_roman in TEST_MAP:
+            self.assertEqual(roman.toRoman(num_arabic), num_roman,
+                             '%s should be %s' % (num_arabic, num_roman))
 
     def test_toRoman_errors(self):
         self.assertRaises(roman.OutOfRangeError, roman.toRoman, 100000)
         self.assertRaises(roman.NotIntegerError, roman.toRoman, '1')
 
     def test_fromRoman(self):
-        self.assertEqual(roman.fromRoman('N'), 0)
-        self.assertEqual(roman.fromRoman('I'), 1)
-        self.assertEqual(roman.fromRoman('MMXIII'), 2013)
+        for num_arabic, num_roman in TEST_MAP:
+            self.assertEqual(roman.fromRoman(num_roman), num_arabic,
+                             '%s should be %s' % (num_roman, num_arabic))
 
     def test_fromRoman_errors(self):
         self.assertRaises(
