8197ecc2bdc24ba374d2fe29ca88e524ee8663ef
Paul Korir <polariseke@gmail.com>
2020-07-06 05:31:49 +0100

    Added CLI and usage instructions
    * CLI command `roman`
    * full test coverage
    * documentation in README.rst
    * updated CHANGES.txt for v3.3
    * version bump: v3.3.dev1

--- src/roman.py
+++ src/roman.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 """Convert to and from Roman numerals"""
 
 __author__ = "Mark Pilgrim (f8dy@diveintopython.org)"
@@ -14,28 +15,40 @@ it under the terms of the Python 2.1.1 license, available at
 http://www.python.org/2.1.1/license.html
 """
 
+import argparse
+import os
 import re
+import sys
 
-#Define exceptions
+
+# Define exceptions
 class RomanError(Exception): pass
+
+
 class OutOfRangeError(RomanError): pass
+
+
 class NotIntegerError(RomanError): pass
+
+
 class InvalidRomanNumeralError(RomanError): pass
 
-#Define digit mapping
-romanNumeralMap = (('M',  1000),
+
+# Define digit mapping
+romanNumeralMap = (('M', 1000),
                    ('CM', 900),
-                   ('D',  500),
+                   ('D', 500),
                    ('CD', 400),
-                   ('C',  100),
+                   ('C', 100),
                    ('XC', 90),
-                   ('L',  50),
+                   ('L', 50),
                    ('XL', 40),
-                   ('X',  10),
+                   ('X', 10),
                    ('IX', 9),
-                   ('V',  5),
+                   ('V', 5),
                    ('IV', 4),
-                   ('I',  1))
+                   ('I', 1))
+
 
 def toRoman(n):
     """convert integer to Roman numeral"""
@@ -55,7 +68,8 @@ def toRoman(n):
             n -= integer
     return result
 
-#Define pattern to detect valid Roman numerals
+
+# Define pattern to detect valid Roman numerals
 romanNumeralPattern = re.compile("""
     ^                   # beginning of string
     M{0,4}              # thousands - 0 to 4 M's
@@ -66,7 +80,8 @@ romanNumeralPattern = re.compile("""
     (IX|IV|V?I{0,3})    # ones - 9 (IX), 4 (IV), 0-3 (0 to 3 I's),
                         #        or 5-8 (V, followed by 0 to 3 I's)
     $                   # end of string
-    """ ,re.VERBOSE)
+    """, re.VERBOSE)
+
 
 def fromRoman(s):
     """convert Roman numeral to integer"""
@@ -83,8 +98,41 @@ def fromRoman(s):
     result = 0
     index = 0
     for numeral, integer in romanNumeralMap:
-        while s[index:index+len(numeral)] == numeral:
+        while s[index:index + len(numeral)] == numeral:
             result += integer
             index += len(numeral)
     return result
 
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        prog='roman',
+        description='convert between roman and arabic numerals'
+    )
+    parser.add_argument('number', help='the value to convert')
+    parser.add_argument('-r', '--reverse',
+                        action='store_true',
+                        default=False,
+                        help='convert roman to numeral (case insensitive) [default: False]')
+
+    args = parser.parse_args()
+    args.number = args.number
+    return args
+
+
+def main():
+    args = parse_args()
+    if args.reverse:
+        u = args.number.upper()
+        r = fromRoman(u)
+        print(r)
+    else:
+        i = int(args.number)
+        n = toRoman(i)
+        print(n)
+
+    return os.EX_OK
+
+
+if __name__ == "__main__":
+    sys.exit(main())
