4840c74fa394cb6caeb36147db131d3825bd5e4f
Jens Vagelpohl <jens@plyp.com>
2024-08-26 14:53:22 +0200

    - update to latest meta/config

--- setup.py
+++ setup.py
@@ -52,7 +52,6 @@ setup(
     python_requires='>=3.8',
     py_modules=["roman"],
     include_package_data=True,
-    test_suite='tests',
     zip_safe=True,
     entry_points={
         'console_scripts': [
