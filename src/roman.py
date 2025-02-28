a9e09a3ec187f009947b19f0c419bab63a185e9a
cecchini <flavio.cecchini@unicatt.it>
2025-02-28 16:33:02 +0100

    Modification to be able to handle quadruple repetitions

--- src/roman.py
+++ src/roman.py
@@ -82,11 +82,11 @@ def toRoman(n):
 romanNumeralPattern = re.compile("""
     ^                   # beginning of string
     M{0,4}              # thousands - 0 to 4 M's
-    (CM|CD|D?C{0,3})    # hundreds - 900 (CM), 400 (CD), 0-300 (0 to 3 C's),
+    (CM|CD|D?C{0,4})    # hundreds - 900 (CM), 400 (CD), 0-300 (0 to 3 C's),
                         #            or 500-800 (D, followed by 0 to 3 C's)
-    (XC|XL|L?X{0,3})    # tens - 90 (XC), 40 (XL), 0-30 (0 to 3 X's),
+    (XC|XL|L?X{0,4})    # tens - 90 (XC), 40 (XL), 0-30 (0 to 3 X's),
                         #        or 50-80 (L, followed by 0 to 3 X's)
-    (IX|IV|V?I{0,3})    # ones - 9 (IX), 4 (IV), 0-3 (0 to 3 I's),
+    (IX|IV|V?I{0,4})    # ones - 9 (IX), 4 (IV), 0-3 (0 to 3 I's),
                         #        or 5-8 (V, followed by 0 to 3 I's)
     $                   # end of string
     """, re.VERBOSE)
